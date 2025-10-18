a5ec263346e16ecc00ad8e2b3efd4d4d2dbf8000
Mahiruu <122439754+mahiru-chanVN@users.noreply.github.com>
2025-10-18 08:01:32 +0700

    Change some info

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # A5BNKND-STEM
+# Author
+Dinh Kien Nguyen (@mahiru-chanVN)
+A5-B NGUYEN KHUYEN NAM DINH
+# Description
 Here is full repo for Arduino bot, using C++ (Sketch)
+
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-// put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
 
 // put function definitions here:
